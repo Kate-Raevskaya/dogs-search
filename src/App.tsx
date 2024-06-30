c0ce1cdc6b55074cedcd2c7ddfb49ab56db2dd3c
Ekaterina <raevskayakatya1707@gmail.com>
2024-06-30 23:08:47 +0300

    try to fix github pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,45 @@ import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom"
 
 import "./App.css"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Outlet></Outlet>,
-    children: [
-      {
-        index: true,
-        element: <p>home</p>,
-      },
-      {
-        path: "signin",
-        element: <p>signin</p>,
-      },
-      {
-        path: "signup",
-        element: <p>signup</p>,
-      },
-      {
-        path: "dogs/:id",
-        element: <p>dogs</p>,
-      },
-      {
-        path: "search",
-        element: <p>search</p>,
-      },
-      {
-        path: "history",
-        element: <p>history</p>,
-      },
-      {
-        path: "favorites",
-        element: <p>favorites</p>,
-      },
-    ],
-  },
-])
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Outlet></Outlet>,
+      children: [
+        {
+          index: true,
+          element: <p>home</p>,
+        },
+        {
+          path: "signin",
+          element: <p>signin</p>,
+        },
+        {
+          path: "signup",
+          element: <p>signup</p>,
+        },
+        {
+          path: "dogs/:id",
+          element: <p>dogs</p>,
+        },
+        {
+          path: "search",
+          element: <p>search</p>,
+        },
+        {
+          path: "history",
+          element: <p>history</p>,
+        },
+        {
+          path: "favorites",
+          element: <p>favorites</p>,
+        },
+      ],
+    },
+  ],
+  { basename: "/aston-react" },
+)
 
 const App = () => {
   return <RouterProvider router={router} />
