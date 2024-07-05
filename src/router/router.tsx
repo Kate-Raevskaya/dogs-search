efe1f7204f13dafef3058266ec9a6a3aadbab532
Ekaterina <raevskayakatya1707@gmail.com>
2024-07-05 22:13:35 +0300

    rename folder

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 
 import App from "../App"
-import { DogPage } from "../pages/DogPage/DogPage"
+import { DogPage } from "../pages/dog-page/DogPage"
 import { Home } from "../pages/home/Home"
 
 export const router = createBrowserRouter([
