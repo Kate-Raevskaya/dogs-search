d3d5d5884a0d655973bf3916722877b752ed1ac4
Ekaterina <raevskayakatya1707@gmail.com>
2024-07-08 20:32:26 +0300

    add suggestions

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,33 +1,17 @@
 import { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
 
+import { getBreedByQuery } from "../../api/dog-api"
 import { BreedsContainer } from "../../components/BreedsContainer"
 import { SearchField } from "../../components/SearchField"
-import type { ApiDog, Dog } from "../../types/dog-types"
-
-async function getBreedByQuery(query: string): Promise<ApiDog[]> {
-  let response = await fetch(
-    `https://api.thedogapi.com/v1/breeds/search?q=${query}`,
-    {
-      headers: {
-        "x-api-key": import.meta.env.VITE_DOG_API_KEY,
-      },
-    },
-  )
-
-  if (response.ok) {
-    return await response.json()
-  } else {
-    throw new Error("Something went wrong!")
-  }
-}
+import type { Dog } from "../../types/dog-types"
 
 export const SearchPage = () => {
   let [searchParams, setSearchParams] = useSearchParams()
   let [dogs, setDogs] = useState<Dog[]>([])
   let [isLoading, setIsLoading] = useState<boolean>(false)
 
-  let query = searchParams.get("breed")
+  let query = searchParams.get("breed") || ""
 
   function handleSubmit(query: string) {
     setSearchParams(new URLSearchParams({ breed: query }))
